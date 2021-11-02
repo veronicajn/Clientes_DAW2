90760eb6f4382d0135aa6313a96e733a96c3be03
veronicajn <veronicajn230285@gmail.com>
2021-11-02 13:39:12 +0100

    Manejo de Strings

--- Manejo_Strings/index.js
+++ Manejo_Strings/index.js
@@ -1,8 +1,20 @@
 
 function ManejoStrings(){
-    let cadena = 'Veronica:Juarez:75778475';
-    let dni;
+    let cadena = 'Veronica:Juarez:123456';
+    let tfo;
     cadena = cadena.toLocaleUpperCase();
     alert(cadena);
 }
 ManejoStrings();
+splitTodosCampos = cadena.split(";");
+split1Campo = cadena.split(":",1);
+alert(splitTodosCampos);
+alert(split1Campo);
+tfo = splitTodosCampos[2];
+//Cambio en el telefono los numeros 3 por 9
+tfo = tfo.replace("2","9");
+alert(tfo);
+
+//Muestra el quinto número del teléfono
+alert(tfo.charArt(4));
+alert("Bienvenido al CEEDCV");
